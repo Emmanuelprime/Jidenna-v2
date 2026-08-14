025e9038f5faf29588af3857102c4aa37862f8ba
Emmanuelprime <emmanuelprime2@gmail.com>
2026-08-14 20:11:46 +0100

    fix

--- drive_square.py
+++ drive_square.py
@@ -2,30 +2,51 @@ import sys
 sys.path.append('..')
 from jidenna.jidenna import Jidenna
 import time
+import math
 
 robot = Jidenna(port='/dev/ttyUSB0')
 robot.connect()
 
+def turn_90_left(robot, turn_speed=0.5):
+    """Turn 90 degrees left"""
+    print("Turning 90 degrees LEFT...")
+    turn_time = (math.pi / 2) / turn_speed  # ~3.14 seconds
+    robot.drive(0, turn_speed)
+    time.sleep(turn_time)
+    robot.stop()
+    time.sleep(0.5)
+
+def move_forward(robot, duration=2, speed=0.2):
+    """Move forward"""
+    print(f"Moving forward for {duration}s...")
+    start_time = time.time()
+    
+    while time.time() - start_time < duration:
+        robot.drive(speed, 0)
+        time.sleep(0.05)
+    
+    robot.stop()
+    time.sleep(0.5)
+
 try:
-    print("Testing turn...")
+    print("Driving in a square...")
     
-    # Test if robot turns with positive angular velocity
-    print("Turning left (positive w)...")
-    robot.drive(0, 0.5)
-    time.sleep(3)
+    for side in range(4):
+        print(f"\n--- Side {side+1} of 4 ---")
+        
+        # Move forward 2 seconds
+        move_forward(robot, duration=2)
+        
+        # Turn left 90 degrees (except after last side)
+        if side < 3:
+            turn_90_left(robot)
+    
+    print("\nSquare complete!")
     robot.stop()
-    time.sleep(1)
-    
-    # Test if robot turns with negative angular velocity
-    print("Turning right (negative w)...")
-    robot.drive(0, -0.5)
-    time.sleep(3)
-    robot.stop()
-    
-    print("Test complete. Did the robot turn?")
     
 except KeyboardInterrupt:
-    print("Interrupted")
+    print("\nInterrupted")
 finally:
     robot.stop()
     robot.disconnect()
+    print("Robot stopped and disconnected")
