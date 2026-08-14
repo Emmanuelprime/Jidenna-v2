d30641b185cacc8a5a2797503af76b1b71c52de0
Emmanuelprime <emmanuelprime2@gmail.com>
2026-08-14 21:22:00 +0100

    added goal

--- test_g2g.py
+++ test_g2g.py
@@ -15,13 +15,13 @@ g2g = G2GController(robot, heading_controller, turning_controller)
 
 try:
     # Test: Go forward 1m
-    g2g.go_to_goal_with_phases(1.0, 0.0, speed=0.2)
+    g2g.go_to_goal_with_phases(1.0, 1.0, speed=0.2)
     
     #Test: Go left 1m
-    g2g.go_to_goal_with_phases(0.0, 1.0, speed=0.2)
+    # g2g.go_to_goal_with_phases(0.0, 1.0, speed=0.2)
     
-    # Test: Go diagonal 1m
-    g2g.go_to_goal_with_phases(0.7, 0.7, speed=0.2)
+    # # Test: Go diagonal 1m
+    # g2g.go_to_goal_with_phases(0.7, 0.7, speed=0.2)
     
 except KeyboardInterrupt:
     print("\nInterrupted")
